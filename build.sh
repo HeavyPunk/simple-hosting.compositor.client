40951eeeb941a2c2e1230974abdbcd4de27c6333
HeavyPunk <kirillkuchma8@gmail.com>
2023-03-27 21:33:47 +0500

    Added binary to repo

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ sbt package
 
 rm -rf $DEST_DIR
 mkdir $DEST_DIR
-cp ./target/scala-3.2.2/simple-hosting-compositor-client_3-0.1.0-SNAPSHOT.jar $DEST_DIR/simple-hosting-compositor-client.jar
+cp ./target/simplehosting-compositor-client-0.1.0-SNAPSHOT.jar $DEST_DIR/simple-hosting-compositor-client.jar
